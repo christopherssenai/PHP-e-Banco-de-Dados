bcb102b839f6d2a7b8add60c7d6a5cdf5173ff37
christophersenai <christopher_steudel@estudante.sesisenai.org.br>
2026-03-30 19:25:38 -0300

    adicionando banco de dados em uso local

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+// Parâmetro 1 : Localhost ---> endereço do server de Banco de Dados.
+// Parâmetro 2 : "root" ---> nome do usuário do MySQL.
+// Parâmetro 3 : "" ---> senha do usuário do MySQL.
+// Parâmetro 4 ; "crud" ---> nome do Banco de Dados.
+$conn = mysqli_connect("localhost", "root", "", "crud");
+
+?>
---
+++ form.php
@@ -0,0 +1,7 @@
+<h2>Cadastro de Usuários</h2>
+
+<form action="salvar.php" method="POST">
+    Nome : <input type="text" name="nome"><br>
+    E-mail : <input type="text" name="email"><br>
+    <button>Salvar</button>
+</form>
